57f8444ed7a0f6826acf9247f6985663086ee1d2
Amancayjesus136 <amancayjesus136@gmail.com>
2023-11-08 05:15:31 -0500

    consulta de reserva en JSON correcto

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -23,43 +23,48 @@ class WelcomeController extends Controller
         $tipoDocumento = $request->input('tipoDocumento');
         $numeroDocumento = $request->input('numeroDocumento');
 
-        $psicologia = DB::table('psicologia')->select('tipo_documento', 'numero_documento')
-                    ->where('tipo_documento', $tipoDocumento)
-                    ->where('numero_documento', $numeroDocumento)
-                    ->first();
+        $psicologia = DB::table('psicologia')
+            ->select('tipo_documento', 'numero_documento', 'nombres', 'apellidos', 'fecha_hora')
+            ->where('tipo_documento', $tipoDocumento)
+            ->where('numero_documento', $numeroDocumento)
+            ->first();
 
-        $terapiaFisica = DB::table('terapia_fisica')->select('tipo_documento', 'numero_documento')
-                        ->where('tipo_documento', $tipoDocumento)
-                        ->where('numero_documento', $numeroDocumento)
-                        ->first();
+        $terapiaFisica = DB::table('terapia_fisica')
+            ->select('tipo_documento', 'numero_documento', 'nombres', 'apellidos', 'fecha_hora')
+            ->where('tipo_documento', $tipoDocumento)
+            ->where('numero_documento', $numeroDocumento)
+            ->first();
 
-        $terapiaInfantil = DB::table('terapia_infantil')->select('tipo_documento', 'numero_documento')
-                        ->where('tipo_documento', $tipoDocumento)
-                        ->where('numero_documento', $numeroDocumento)
-                        ->first();
+        $terapiaInfantil = DB::table('terapia_infantil')
+            ->select('tipo_documento', 'numero_documento', 'nombres', 'apellidos', 'fecha_hora')
+            ->where('tipo_documento', $tipoDocumento)
+            ->where('numero_documento', $numeroDocumento)
+            ->first();
 
-        $terapiaLenguaje = DB::table('terapia_lenguaje')->select('tipo_documento', 'numero_documento')
-                        ->where('tipo_documento', $tipoDocumento)
-                        ->where('numero_documento', $numeroDocumento)
-                        ->first();
+        $terapiaLenguaje = DB::table('terapia_lenguaje')
+            ->select('tipo_documento', 'numero_documento', 'nombres', 'apellidos', 'fecha_hora')
+            ->where('tipo_documento', $tipoDocumento)
+            ->where('numero_documento', $numeroDocumento)
+            ->first();
 
-        $terapiaOcupacional = DB::table('terapia_ocupacional')->select('tipo_documento', 'numero_documento')
-                            ->where('tipo_documento', $tipoDocumento)
-                            ->where('numero_documento', $numeroDocumento)
-                            ->first();
+        $terapiaOcupacional = DB::table('terapia_ocupacional')
+            ->select('tipo_documento', 'numero_documento', 'nombres', 'apellidos', 'fecha_hora')
+            ->where('tipo_documento', $tipoDocumento)
+            ->where('numero_documento', $numeroDocumento)
+            ->first();
+
+        $resultados = [];
 
         if ($psicologia) {
-            $resultados = ['psicologia' => $psicologia];
+            $resultados['psicologia'] = $psicologia;
         } elseif ($terapiaFisica) {
-            $resultados = ['terapia_fisica' => $terapiaFisica];
+            $resultados['terapia_fisica'] = $terapiaFisica;
         } elseif ($terapiaInfantil) {
-            $resultados = ['terapia_infantil' => $terapiaInfantil];
+            $resultados['terapia_infantil'] = $terapiaInfantil;
         } elseif ($terapiaLenguaje) {
-            $resultados = ['terapia_lenguaje' => $terapiaLenguaje];
+            $resultados['terapia_lenguaje'] = $terapiaLenguaje;
         } elseif ($terapiaOcupacional) {
-            $resultados = ['terapia_ocupacional' => $terapiaOcupacional];
-        } else {
-            $resultados = []; 
+            $resultados['terapia_ocupacional'] = $terapiaOcupacional;
         }
 
         return response()->json($resultados);
