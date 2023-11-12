68d1bb5141031f744a864f0425932501a9f48d1b
pot2799 <jordan.potenciano@ismem.edu.pe>
2023-11-12 14:39:37 -0500

    12344567

--- resources/views/home/welcome.blade.php
+++ resources/views/home/welcome.blade.php
@@ -1317,7 +1317,7 @@
     <div class="modal-dialog modal-xl">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="crearModalLabel">Terapia infantil</h5>
+                <h5 class="modal-title" id="crearModalLabel">Equipos de técnologias</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Cerrar"></button>
             </div>
             <div class="modal-body">
