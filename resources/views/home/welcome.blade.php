0d8e7ddf1a328c43cf0dd1c92fff1c5585045b64
Amancayjesus136 <amancayjesus136@gmail.com>
2023-11-25 16:58:00 -0500

    MODIFICAR GUSTO de modal

--- resources/views/home/welcome.blade.php
+++ resources/views/home/welcome.blade.php
@@ -301,8 +301,8 @@
                                     </div>
                                 </form>
 
-                                <div class="modal fade" id="resultadoModal" tabindex="-1" aria-labelledby="resultadoModalLabel" aria-hidden="true">
-                                    <div class="modal-dialog modal-lg">
+                                <<div class="modal fade fadeInRight" id="resultadoModal" tabindex="-1" aria-labelledby="resultadoModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog modal-sm">
                                         <div class="modal-content">
                                             <div class="modal-header">
                                                 <h5 class="modal-title" id="resultadoModalLabel">Resultados de Consulta</h5>
