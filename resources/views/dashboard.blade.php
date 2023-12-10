5a480116146df7da4812d791944ba1aaec23c974
Amancayjesus136 <amancayjesus136@gmail.com>
2023-12-10 04:36:05 -0500

    false clock

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -48,7 +48,7 @@ $porcentajeTerapiaLenguaje = ($totalFilas != 0) ? ($clienteFilasTerapiaLenguaje 
                         <div class="row g-3 mb-0 align-items-center">
                             <div class="col-sm-auto">
                                 <div class="input-group">
-                                <input type="text" class="form-control border-0 dash-filter-picker shadow" data-provider="flatpickr" data-range-date="true" data-date-format="d M, Y h:i K" data-default-date="01 Jan 2022 to 31 Jan 2022" id="fechaInput">
+                                <input type="text" class="form-control border-0 dash-filter-picker shadow" data-provider="" data-date-format="d M, Y h:i K" id="fechaInput">
                                     <div class="input-group-text bg-primary border-primary text-white">
                                         <i class="ri-calendar-2-line"></i>
                                     </div>
